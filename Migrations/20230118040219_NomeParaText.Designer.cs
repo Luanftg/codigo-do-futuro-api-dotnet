14e2e6ca193e0cf262aab8f1faca5481b5110f80
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-18 20:23:37 -0300

    [migration]: atualiza campo nome para texto

--- Migrations/20230118040219_NomeParaText.Designer.cs
+++ Migrations/20230118040219_NomeParaText.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using cdf_api_integrador.Repositories.Entity;
 
@@ -10,9 +11,11 @@ using cdf_api_integrador.Repositories.Entity;
 namespace cdfapiintegrador.Migrations
 {
     [DbContext(typeof(ContextEntity))]
-    partial class ContextEntityModelSnapshot : ModelSnapshot
+    [Migration("20230118040219_NomeParaText")]
+    partial class NomeParaText
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
