683a848b0ce1a7eda47ca162aa527e80feacea88
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-17 21:26:44 -0300

    [refact]: inclui Regra no Usuario

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,11 +1,11 @@
-namespace cdf_api_integrador.Model;
+namespace cdf_api_integrador.Models;
 
 public record Usuario
 {
     int Id {get;set;}= default!;
     string Email {get;set;} = default!;
     string Senha {get;set;} = default!;
+    string Regra {get;set;} = default!;
     decimal Nome {get;set;} = default!;
-  
 
 }
