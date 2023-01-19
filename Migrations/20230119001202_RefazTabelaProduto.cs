4ce2388968fcc73755cf621f828746d65600e80f
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-18 21:16:53 -0300

    [Migration]: atualiza tabela produto

---
+++ Migrations/20230119001202_RefazTabelaProduto.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cdfapiintegrador.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefazTabelaProduto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dt_Criacao",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "Photo_Url",
+                table: "Produtos");
+
+            migrationBuilder.RenameColumn(
+                name: "Loja_Id",
+                table: "Produtos",
+                newName: "QtdEstoque");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Valor",
+                table: "Produtos",
+                type: "decimal(65,30)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateTable(
+                name: "Campanhas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    LojaId = table.Column<int>(name: "Loja_Id", type: "int", nullable: false),
+                    Nome = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Descricao = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DtCriacao = table.Column<DateTime>(name: "Dt_Criacao", type: "datetime(6)", nullable: false),
+                    PhotoUrl = table.Column<string>(name: "Photo_Url", type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Campanhas", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Campanhas");
+
+            migrationBuilder.DropColumn(
+                name: "Valor",
+                table: "Produtos");
+
+            migrationBuilder.RenameColumn(
+                name: "QtdEstoque",
+                table: "Produtos",
+                newName: "Loja_Id");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Dt_Criacao",
+                table: "Produtos",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Photo_Url",
+                table: "Produtos",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
