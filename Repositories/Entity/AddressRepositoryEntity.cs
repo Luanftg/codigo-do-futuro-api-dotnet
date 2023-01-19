9351a5b62220f564549ffcc911ceff7d881a64e8
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-19 11:02:23 -0300

    [feat]: cria endPoint de Endereço - DTO,Repository e Controlador

---
+++ Repositories/Entity/AddressRepositoryEntity.cs
@@ -0,0 +1,44 @@
+
+
+using cdf_api_integrador.Models;
+
+using cdf_api_integrador.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace cdf_api_integrador.Repositories.Entity;
+
+public class AddressRepositoryEntity : IRepository<Endereco>
+{
+    private ContextEntity context;
+    public AddressRepositoryEntity()
+    {
+        context = new ContextEntity();
+    }
+
+    public async Task<List<Endereco>> TodosAsync()
+    {
+        return await context.Enderecos.ToListAsync();
+    }
+
+    public async Task IncluirAsync(Endereco endereco)
+    {
+        context.Enderecos.Add(endereco);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task<Endereco> AtualizarAsync(Endereco endereco)
+    {
+        context.Entry(endereco).State = EntityState.Modified;
+        await context.SaveChangesAsync();
+
+        return endereco;
+    }
+
+    public async Task ApagarAsync(Endereco endereco)
+    {
+        var obj = await context.Enderecos.FindAsync(endereco.Id);
+        if(obj is null) throw new Exception("Endereco não encontrado");
+        context.Enderecos.Remove(obj);
+        await context.SaveChangesAsync();
+    }
+}
