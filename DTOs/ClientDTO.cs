08dd1a50168dd5e8d68c5391d84222e1a16c1490
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-19 11:01:52 -0300

    [feat]: cria endPoint de Cliente - DTO,Repository e Controlador

---
+++ DTOs/ClientDTO.cs
@@ -0,0 +1,10 @@
+namespace cdf_api_integrador.DTOs;
+
+public record ClientDTO
+{
+    public string Nome {get;set;} = default!;
+    public string Telefone {get;set;} = default!;
+    public string Email {get;set;} = default!;
+    public string Cpf {get;set;} = default!;
+    public string Endereco_Id {get;set;}= default!;
+}
