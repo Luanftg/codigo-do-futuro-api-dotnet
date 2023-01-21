23c0b199ea153fef8331515c2e95605adf8b7ac9
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-21 15:48:25 -0300

    [refact]: corrige DTOS

--- DTOs/StoreDTO.cs
+++ DTOs/StoreDTO.cs
@@ -3,6 +3,7 @@ namespace cdf_api_integrador.DTOs;
 public record StoreDTO
 {
     public string Nome {get;set;} = default!;
+    public string PhotoUrl {get;set;} = default!;
     public string Descricao {get;set;} = default!;
     public decimal Valor {get;set;} = default!;
     public int QtdEstoque {get;set;} = default!;
--- DTOs/UserLoginDTO.cs
+++ DTOs/UserLoginDTO.cs
@@ -4,6 +4,4 @@ public record UserLoginDTO
 {
     public string Email {get;set;} = default!;
     public string Senha {get;set;} = default!;
-    public string Regra {get;set;} = default!;
-    public string Nome {get;set;} = default!;
 }
