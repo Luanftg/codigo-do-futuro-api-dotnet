6302acff3c2a1c20207f44ecbaeaea69fa6ce9b9
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-01-21 13:57:00 -0300

    [refact]: inclui chave estrangeira e corrige nome da coluna

--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -6,7 +6,8 @@ public record Pedido
 {
     [Key]
     public int Id {get;set;}= default!;
-    public int Cliente_Id {get;set;} = default!;
+    public int ClienteId {get;set;} = default!;
+    public Cliente? Cliente {get;set;}
     public decimal ValorTotal {get;set;} = default!;
     public DateTime DtCriacao {get;set;} = default!;
 
--- Models/PedidoProduto.cs
+++ Models/PedidoProduto.cs
@@ -8,8 +8,10 @@ public record PedidoProduto
 {
     [Key]
     public int Id {get;set;} = default!;
-    public int Produto_Id {get;set;} = default!;
+    public int ProdutoId {get;set;} = default!;
+    public Produto? Produto {get;set;}
+    public int PedidoId {get;set;} = default!;
+    public Pedido? Pedido {get;set;}
     public int Quantidade {get;set;} = default!;
     public decimal Valor {get;set;} = default!;
-    public int Pedido_Id {get;set;} = default!;
 }
